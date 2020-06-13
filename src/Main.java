165fdefb06fe89262f473286cd736bff477f8ccc
Gustavo780904 <gustavodutrasantos@gmail.com>
2020-06-13 12:02:41 -0300

    Projeto finalizado

--- src/Main.java
+++ src/Main.java
@@ -6,22 +6,24 @@ public class Main {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
-		int N = 0, soma = 1;
-		
+
+		int N = 0, soma = 0;
+
 		N = sc.nextInt();
-				
-		for (int i = 0; i < N; i ++) {
-			soma = soma + 1;
-			if (soma % 4 != 0) {
-			System.out.print(soma + " ");
-			}else {
-			System.out.println("PUM");
+
+		for (int i = 0; i < N; i++) {
+
+			for (int x = 0; x < 4; x++) {
+				soma = soma + 1;
+				if (soma % 4 != 0) {
+					System.out.print(soma + " ");
+				} else {
+					System.out.println("PUM");
+				}
 			}
-			
+
 		}
-		
-		
+
 		sc.close();
 
 	}
